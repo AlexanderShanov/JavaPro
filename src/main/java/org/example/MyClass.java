876a1b139b74fd672df328c258ec27cd0348edde
Alexander Shanov <shanov@pointcad.ru>
2023-06-30 19:36:42 +0300

    fristGradle

---
+++ src/main/java/org/example/MyClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MyClass {
+    private int i;
+    public MyClass(int i){
+        this.i = i;
+    }
+    public int getI(){
+        return i + 1;
+    }
+}
---
+++ src/test/java/MyClassTest.java
@@ -0,0 +1,33 @@
+import org.example.MyClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+public class MyClassTest {
+    @Test
+    void Test1(){
+        List<String> list = new ArrayList<>();
+        List<String> spyOnList = spy(list);
+
+        when(spyOnList.size()).thenReturn(10);
+        Assertions.assertEquals(10, spyOnList.size());
+
+        //calling real methods since below methods are not stubbed
+        spyOnList.add("Pankaj");
+        spyOnList.add("Meghna");
+        Assertions.assertEquals("Pankaj", spyOnList.get(0));
+
+        Assertions.assertEquals("Meghna", spyOnList.get(1));
+    }
+    @Test
+    public void test2(){
+        Assertions.assertEquals(true, true);
+        Assertions.assertEquals(true, true);
+    }
+}
